5f9042f1510717f7d01036aae2e69ddf07f8dd9e
RyukaST077 <rynsk2tym@icloud.com>
2026-02-19 15:59:11 +0900

    初期化

--- src/app/history/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HistoryPage() {
-  return (
-    <main>
-      <h1>SCR-005 履歴カレンダー</h1>
-      <p>/history</p>
-    </main>
-  );
-}
--- src/server/infrastructure/repositories/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './types';
-export * from './UserRepository';
-export * from './HabitRepository';
-export * from './PolicyRepository';
-export * from './OpsRepository';
-
